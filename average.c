851dd46eeca0a2273fcb9db03e3e5d72ec90abc8
imnotomkar <omkartuplondhe721@gmail.com>
2025-11-22 22:47:52 +0530

    Add files via upload

---
+++ average.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(){
+    int a,b,c,avg;
+
+    printf("enter the number 1:");
+    scanf("%d",&a);
+
+    printf("enter the number 2:");
+    scanf("%d",&b);
+
+    printf("enter the number 3:");
+    scanf("%d",&c);
+
+    avg=(a+b+c)/3;
+    printf("the avg is:%d\n",avg);
+
+    return 0;
+
+
+}
---
+++ average.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b,c,avg;
+
+    cout<<"enter the number1: ";
+    cin>>a;
+
+    cout<<"enter the number2: ";
+    cin>>b;
+
+    cout<<"enter the number3: ";
+    cin>>c;
+
+    avg=(a+b+c)/3;
+
+    cout<<avg;
+
+
+
+
+}
