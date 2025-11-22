d5b1b96ec2966174a8a8f9db0d13f155b796b30c
imnotomkar <omkartuplondhe721@gmail.com>
2025-11-22 22:18:27 +0530

    Add files via upload

---
+++ Positive&Negative.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+    cout<<"Enter the number:";
+    cin>>num;
+
+    if(num>0)
+    {
+        cout<<"Number is positive";
+    }
+    else
+    {
+        cout<<"number is negative";
+    }
+
+    return 0;
+}
